6d658a33092a1c31b40b40e9f93066a30d42a1bc
mikan-megane <kanata702@gmail.com>
2016-02-21 16:18:42 +0900

    realpath

--- index.php
+++ index.php
@@ -18,9 +18,7 @@
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
 	<?php
-		$myurl = "http://mikan-php.pe.hu/";
 		function image_generater($image){
-			global $myurl;
 			if (@$_POST["submit"]) {
 				$im = ImageCreateFromJPEG( "./image/${image}.jpg" );
 				$color = imagecolorallocate( $im, 63, 63, 255 );
@@ -30,9 +28,9 @@
 
 				imagecolordeallocate( $im,$color );
 				imagedestroy( $im );
-				return "${myurl}generate/${image}.png";
+				echo realpath("generate/${image}.png");
 			} else {
-				return "${myurl}image/${image}.png";
+				echo realpath("image/${image}.png");
 			}
 		}
 		if ($submit == "作成") {
