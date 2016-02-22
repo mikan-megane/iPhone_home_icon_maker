97ddedd73e91a0f67ee6ecc08683c2e89c3076f4
mikan-megane <kanata702@gmail.com>
2016-02-22 22:27:51 +0900

    button

--- index.php
+++ index.php
@@ -152,10 +152,7 @@
 				</div>
 				<div class="raw">
 					<div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
-						<div class="input-group">
-						  <span class="input-group-addon"><span class="glyphicon glyphicon-search"></span></span>
-						  <input class="btn btn-primary form-control" type="submit" name="submit" value="プレビュー">
-						</div>
+						  <button class="btn btn-primary form-control" type="submit" name="submit" value="プレビュー"><span class="glyphicon glyphicon-search"></span>プレビュー</button>
 					</div>
 					<div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
 						<input class="btn btn-success form-control" type="submit" name="submit" value="作成"><span class="glyphicon glyphicon-search"></span></input>
